faa0a6d326b64ee3d39de4324b02390ccf24ad63
marcos <marcgabe@myuax.com>
2024-10-06 16:27:08 +0200

    Nuevas clase de prueva
    Se han creado un par de clases en base a las peticiones de la modificacion adaptada a firebase que de momento no usamos especialmente

---
+++ app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/service/CargaNovelas.java
@@ -0,0 +1,45 @@
+package com.example.feedback1_aplicaciondegestiondenovelass.service;
+
+import android.content.Context;
+import androidx.loader.content.AsyncTaskLoader;
+import com.example.feedback1_aplicaciondegestiondenovelass.modelo.Novel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CargaNovelas extends AsyncTaskLoader<List<Novel>> {
+
+    public CargaNovelas(Context context) {
+        super(context);
+    }
+
+    @Override
+    public List<Novel> loadInBackground() {
+        List<Novel> novels = new ArrayList<>();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("novels");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Novel novel = snapshot.getValue(Novel.class);
+                    novels.add(novel);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Este por ahora nada
+            }
+        });
+        return novels;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+}
