8c0f46029fe73fdfb5af46311cc74e6010e96c07
marcos <marcgabe@myuax.com>
2024-10-07 18:15:24 +0200

    Simplificacion del Main
    como las clases service que he creado de moemnto no las uso para una conexion "sana" con firebase estabezco una version mas prematura y sencillas de main (comentada /* */ la version mas completa justo debajo)

--- app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/MainActivity.kt
+++ app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/MainActivity.kt
@@ -1,5 +1,37 @@
 package com.example.feedback1_aplicaciondegestiondenovelass
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.feedback1_aplicaciondegestiondenovelass.ui.theme.Feedback1AplicacióndeGestióndeNovelassTheme
+import com.example.feedback1_aplicaciondegestiondenovelass.vista.PantallaPrincipal
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Feedback1AplicacióndeGestióndeNovelassTheme {
+                val navController = rememberNavController()
+                NavHost(navController, startDestination = "main") {
+                    composable("main") { PantallaPrincipal() }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+/*
+package com.example.feedback1_aplicaciondegestiondenovelass
+
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.app.job.JobInfo
@@ -17,13 +49,12 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.feedback1_aplicaciondegestiondenovelass.ui.theme.Feedback1AplicacióndeGestióndeNovelassTheme
 import com.example.feedback1_aplicaciondegestiondenovelass.vista.PantallaPrincipal
-import com.example.feedback1_aplicaciondegestiondenovelass.service.SyncJobService
+//import com.example.feedback1_aplicaciondegestiondenovelass.service.SyncJobService
 import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Llama a setPersistenceEnabled antes de cualquier otra operación con Firebase
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -36,9 +67,12 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        scheduleJob(this)
-        scheduleAlarm()
+        //scheduleJob(this)
+        //scheduleAlarm()
     }
+}
+
+
 
     companion object {
         @JvmStatic
@@ -66,3 +100,4 @@ class MainActivity : ComponentActivity() {
         )
     }
 }
+*/
