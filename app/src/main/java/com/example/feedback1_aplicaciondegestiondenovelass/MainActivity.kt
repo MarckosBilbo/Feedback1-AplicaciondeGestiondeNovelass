4d34dace4ac8a9578ee5fdf28da1504a5afb73a8
marcos <marcgabe@myuax.com>
2024-10-21 17:22:45 +0200

    Update MainActivity.kt
    Borrando comentarios masivos e inutiles y poco mas ciertamente en esta cambio

--- app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/MainActivity.kt
+++ app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.feedback1_aplicaciondegestiondenovelass.ui.theme.Feedback1AplicacióndeGestióndeNovelassTheme
 import com.example.feedback1_aplicaciondegestiondenovelass.vista.PantallaPrincipal
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,32 +30,13 @@ class MainActivity : ComponentActivity() {
 
 
 /*
-package com.example.feedback1_aplicaciondegestiondenovelass
-
-import android.app.AlarmManager
-import android.app.PendingIntent
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.*
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.feedback1_aplicaciondegestiondenovelass.ui.theme.Feedback1AplicacióndeGestióndeNovelassTheme
-import com.example.feedback1_aplicaciondegestiondenovelass.vista.PantallaPrincipal
-//import com.example.feedback1_aplicaciondegestiondenovelass.service.SyncJobService
-import com.google.firebase.database.FirebaseDatabase
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -67,12 +48,8 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        //scheduleJob(this)
-        //scheduleAlarm()
+        scheduleJob(this)
     }
-}
-
-
 
     companion object {
         @JvmStatic
@@ -81,23 +58,12 @@ class MainActivity : ComponentActivity() {
             val jobInfo = JobInfo.Builder(1, componentName)
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
                 .setPersisted(true)
-                .setPeriodic(15 * 60 * 1000) // Con quince minutitos creo que bien
+                .setPeriodic(15 * 60 * 1000)
                 .build()
 
             val jobScheduler = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
             jobScheduler.schedule(jobInfo)
         }
-    }
-
-
-    private fun scheduleAlarm() {
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(this, SyncJobService::class.java)
-        val pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-        val interval = AlarmManager.INTERVAL_FIFTEEN_MINUTES
-        alarmManager.setInexactRepeating(
-            AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + interval, interval, pendingIntent
-        )
     }
 }
 */
