e7907f2324343df5c904a84d68a388bdb1b0d3f5
marcos <marcgabe@myuax.com>
2024-10-23 20:11:58 +0200

    Update Theme.kt
    Nada un salto hago el commit para que deje de molestar en la intefaz del desktop

--- app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/ui/theme/Theme.kt
+++ app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.feedback1_aplicaciondegestiondenovelass.ui.theme
 
-
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
