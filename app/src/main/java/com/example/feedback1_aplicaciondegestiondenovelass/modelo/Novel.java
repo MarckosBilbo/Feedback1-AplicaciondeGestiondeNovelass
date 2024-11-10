e554305f4364efd2e2b2e14c75621a8a2d3fe492
marcos <marcgabe@myuax.com>
2024-11-10 22:28:40 +0100

    Update Novel.java
    Metemos tras el tema de los fragmentos el correspondiente adaptado de los datos con parcelable , manteniendo la orientacion original y permitiendo con lo nuevo que los fragmentos juegan con los datos tambien

--- app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/modelo/Novel.java
+++ app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/modelo/Novel.java
@@ -1,6 +1,10 @@
+// Novel.java
 package com.example.feedback1_aplicaciondegestiondenovelass.modelo;
 
-public class Novel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Novel implements Parcelable {
     private String key;
     private String title;
     private String author;
@@ -9,7 +13,7 @@ public class Novel {
     private boolean isFavorite;
 
     public Novel() {
-        // Constructor sin argumentos para Firebase
+        // Default constructor required for calls to DataSnapshot.getValue(Novel.class)
     }
 
     public Novel(String title, String author, int year, String synopsis) {
@@ -20,16 +24,53 @@ public class Novel {
         this.isFavorite = false;
     }
 
+    protected Novel(Parcel in) {
+        key = in.readString();
+        title = in.readString();
+        author = in.readString();
+        year = in.readInt();
+        synopsis = in.readString();
+        isFavorite = in.readByte() != 0;
+    }
+
+    public static final Creator<Novel> CREATOR = new Creator<Novel>() {
+        @Override
+        public Novel createFromParcel(Parcel in) {
+            return new Novel(in);
+        }
+
+        @Override
+        public Novel[] newArray(int size) {
+            return new Novel[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(key);
+        dest.writeString(title);
+        dest.writeString(author);
+        dest.writeInt(year);
+        dest.writeString(synopsis);
+        dest.writeByte((byte) (isFavorite ? 1 : 0));
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    // Getters and setters
     public String getKey() { return key; }
     public void setKey(String key) { this.key = key; }
     public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public int getYear() { return year; }
-    public String getSynopsis() { return synopsis; }
-    public boolean isFavorite() { return isFavorite; }
     public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
     public void setAuthor(String author) { this.author = author; }
+    public int getYear() { return year; }
     public void setYear(int year) { this.year = year; }
+    public String getSynopsis() { return synopsis; }
     public void setSynopsis(String synopsis) { this.synopsis = synopsis; }
+    public boolean isFavorite() { return isFavorite; }
     public void setFavorite(boolean favorite) { isFavorite = favorite; }
 }
