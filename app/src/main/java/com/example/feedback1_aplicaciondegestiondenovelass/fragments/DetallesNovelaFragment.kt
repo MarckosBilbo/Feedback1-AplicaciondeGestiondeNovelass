b8a28d0ff7da2a3ed982f9ab3c3b34d78befaaf6
marcos <marcgabe@myuax.com>
2024-11-13 15:04:51 +0100

    Recurso
    Los dejamos en su paquete propio como recurso a futuro en caso de readpatacion de contexto

---
+++ app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/fragments/DetallesNovelaFragment.kt
@@ -0,0 +1,25 @@
+package com.example.feedback1_aplicaciondegestiondenovelass.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.feedback1_aplicaciondegestiondenovelass.modelo.Novel
+import com.example.feedback1_aplicaciondegestiondenovelass.vista.NovelDetails
+
+class DetallesNovelaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val novel = arguments?.getParcelable<Novel>("novel")
+        return ComposeView(requireContext()).apply {
+            setContent {
+                novel?.let { NovelDetails(it) }
+            }
+        }
+    }
+}
