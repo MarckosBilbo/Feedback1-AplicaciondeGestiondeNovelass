a298c6d702a7f9ae6b0943b48251f38ec153795c
marcos <marcgabe@myuax.com>
2024-11-26 10:33:07 +0100

    Update RedOptima.java
    Le metemos la descompresion que sino no cuaja en firebase y te chapa

--- app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/util/RedOptima.java
+++ app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/util/RedOptima.java
@@ -1,12 +1,16 @@
 package com.example.feedback1_aplicaciondegestiondenovelass.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 
 //Para comprimir los datos de la red y que la aplicación sea más rápida (para el PROFILER punto 2)
 public class RedOptima {
+
+
     public static byte[] comprimirDatos(byte[] datos) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
@@ -14,4 +18,23 @@ public class RedOptima {
         gzipOutputStream.close();
         return byteArrayOutputStream.toByteArray();
     }
+
+
+
+    public static byte[] descomprimirDatos(byte[] datos) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(datos);
+        GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = gzipInputStream.read(buffer)) != -1) {
+            byteArrayOutputStream.write(buffer, 0, len);
+        }
+
+        gzipInputStream.close();
+        byteArrayOutputStream.close();
+        return byteArrayOutputStream.toByteArray();
+    }
+
 }
