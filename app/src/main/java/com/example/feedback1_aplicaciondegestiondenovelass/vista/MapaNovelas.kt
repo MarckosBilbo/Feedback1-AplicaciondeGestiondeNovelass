e084bd14040765be727b410c34d898cc4ffd81d6
marcos <marcgabe@myuax.com>
2024-12-13 18:52:38 +0100

    Create MapaNovelas.kt
    Mapeado que te muestra iconizadas las novelas creadas por el ususario como siempre desde la app

---
+++ app/src/main/java/com/example/feedback1_aplicaciondegestiondenovelass/vista/MapaNovelas.kt
@@ -0,0 +1,44 @@
+package com.example.feedback1_aplicaciondegestiondenovelass.vista
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.example.feedback1_aplicaciondegestiondenovelass.modelo.VistaModeloNovela
+import com.example.feedback1_aplicaciondegestiondenovelass.modelo.Novel
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+
+@Composable
+fun MapaNovelas(navController: NavHostController) {
+    val viewModel: VistaModeloNovela = viewModel()
+    val novels by viewModel.getAllNovels().observeAsState(emptyList())
+    val context = LocalContext.current
+    val mapView = remember { MapView(context) }
+
+    AndroidView(factory = { mapView }) {
+        mapView.setTileSource(org.osmdroid.tileprovider.tilesource.TileSourceFactory.MAPNIK)
+        mapView.setBuiltInZoomControls(true)
+        mapView.controller.setZoom(2.0)
+
+        novels.forEach { novel: Novel ->
+            val marker = Marker(mapView)
+            marker.position = GeoPoint(novel.latitude, novel.longitude)
+            marker.title = novel.title
+            mapView.overlays.add(marker)
+        }
+    }
+
+    Button(onClick = { navController.popBackStack() }) {
+        Text("Volver a la Principal")
+    }
+}
+
+
